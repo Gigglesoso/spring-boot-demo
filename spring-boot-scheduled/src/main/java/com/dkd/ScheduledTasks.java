a07efa85607514aa4c813e2ec52e69a9a9ac6bab
Gigglesoso <469920680@163.com>
2018-08-02 16:42:05 +0800

    Update ScheduledTasks.java

--- spring-boot-scheduled/src/main/java/com/dkd/ScheduledTasks.java
+++ spring-boot-scheduled/src/main/java/com/dkd/ScheduledTasks.java
@@ -11,6 +11,7 @@ public class ScheduledTasks {
 	
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 	
+	
 //    @Scheduled(fixedRate = 5000) ：上一次开始执行时间点之后5秒再执行
 //    @Scheduled(fixedDelay = 5000) ：上一次执行完毕时间点之后5秒再执行
 //    @Scheduled(initialDelay=1000, fixedRate=5000) ：第一次延迟1秒后执行,后按fixedRate的规则每5秒执行一次
