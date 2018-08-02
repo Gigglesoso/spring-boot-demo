565ac9ba4080afbe6b18b572b25d6b6d4196b38e
gaoxp <gaoxp-dkd@canic.com>
2018-08-02 16:56:03 +0800

    springboot定时任务

---
+++ spring-boot-scheduled/src/main/java/com/dkd/SpringBootScheduledApplication.java
@@ -0,0 +1,13 @@
+package com.dkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling  //开启定时任务
+public class SpringBootScheduledApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootScheduledApplication.class, args);
+	}
+}
